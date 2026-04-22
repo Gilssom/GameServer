0b42a66ac788e9419cd3667c9c5c488c733deaf9
GilSsom <gil5299@naver.com>
2026-04-22 18:04:59 +0900

    Select Model
    Select Model, Event Select Model

--- GameServer/GameServer/GameServer.cpp
+++ GameServer/GameServer/GameServer.cpp
@@ -19,6 +19,15 @@ void HandleError(const char* cause)
 	cout << "Send Error Code = " << errCode << endl;
 }
 
+const int32 BUFSIZE = 1000;
+struct Session
+{
+	SOCKET socket = INVALID_SOCKET;
+	char recvBuffer[BUFSIZE] = {};
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
+};
+
 int main()
 {
 	WSAData wsaData;
@@ -55,62 +64,146 @@ int main()
 
 	cout << "Accept" << endl;
 
-	SOCKADDR_IN clientAddr;
-	int32 addrLen = sizeof(clientAddr);
+	// WSAEventSelect = (WSAEventSelect 함수가 핵심이 되는)
+	// 소켓과 관련된 네트워크 이벤트를 [이벤트 객체]를 통해 감지	
+	
+	// 이벤트 객체 관련 함수들
+	// 생성 : WSACreateEvent (수동 리셋 Manual-Reset + Non-Signaled 상태 시작)
+	// 삭제 : WSACloseEvent
+	// 신호 상태 감지 : WSAWaitForMultipleEvents
+	// 구체적인 네트워크 이벤트 알아내기 : WSAEnumNetworkEvents
+
+	// 소켓 <-> 이벤트 객체 연동
+	// WSAEventSelect(socket, event, networkEvents);
+	// - 관심있는 네트워크 이벤트
+	// FD_ACCEPT : 접속한 클라가 있음 : accept
+	// FD_READ : 데이터 수신 가능 recv, recvfrom
+	// FD_WRITE : 데이터 송신 가능 send, sendto
+	// FD_CLOSE : 상대가 접속 종료
+	// FD_CONNECT : 통신을 위한 연결 절차 완료
+	// FD_OOB
+
+	// 주의 사항
+	// WSAEventSelect 함수를 호출하면, 해당 소켓은 자동으로 Non-Blocking Mode 로 전환됨
+	// accept() 함수가 반환하는 소켓은 listenSocket 과 동일한 속성을 갖게됨
+	// - 따라서 clientSocket 은 FD_READ, FD_WRITE 등을 다시 등록해야함
+	// 드물게 WSAEWOULDBLOCK 오류가 뜰 수 있으니 예외 처리가 필요함
+	// 중요)
+	// - 이벤트 발생 시, 적절한 소켓 함수 호출해야 함
+	// - 아니면 다음 번에는 동일 네트워크 이벤트가 발생하지 않음 
+	// - ex) FD_READ 이벤트가 떴으면, recv 를 호출해야한다 (안할 시 FD_READ 두번 다시 안뜸)
+
+	// 1) count, event
+	// 2) waitAll : 모두 기다릴 것인지? 아니면 하나만 완료되어도?
+	// 3) timeout : 타임아웃
+	// 4) 지금은 false 설정
+	// return : 완료된 첫번째 Idx
+	// WSAWaitForMultipleEvents
+
+	// 1) socket
+	// 2) eventObject : socket 과 연동된 이벤트 객체 핸들을 넘겨주면, 이벤트 객체를 Non-Signaled
+	// 3) networkEvent : 네트워크 이벤트 / 오류 정보가 저장
+	// WSAEnumNetworkEvents
+
+	// wsaEvents - sessions = 1:1 대응 필요
+	vector<WSAEVENT> wsaEvents;
+	vector<Session> sessions;
+	sessions.reserve(100);
+
+	WSAEVENT listenEvent = ::WSACreateEvent();
+	wsaEvents.push_back(listenEvent);
+	sessions.push_back(Session{ listenSocket });
+	if (::WSAEventSelect(listenSocket, listenEvent, FD_ACCEPT | FD_CLOSE) == SOCKET_ERROR)
+		return 0;
 
 	while (true)
 	{
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
+		int32 Idx = ::WSAWaitForMultipleEvents(wsaEvents.size(), &wsaEvents[0], FALSE, WSA_INFINITE, FALSE);
+		if (Idx == WSA_WAIT_FAILED)
+			continue;
+
+		Idx -= WSA_WAIT_EVENT_0;
+
+		//::WSAResetEvent(wsaEvents[Idx]); -> 아래 WSAEnumNetworkEvents 함수에서 어차피 호출함
+
+		WSANETWORKEVENTS networkEvents;
+		if (::WSAEnumNetworkEvents(sessions[Idx].socket, wsaEvents[Idx], &networkEvents) == SOCKET_ERROR)
+			continue;
+
+		// Listener Socket Check
+		if (networkEvents.lNetworkEvents & FD_ACCEPT)
 		{
-			// 원래 블록했어야 하는 상황인데, 논블로킹으로 설정을 했다면
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			// Error-Check
+			if (networkEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
 				continue;
 
-			// Error
-			break;
+			SOCKADDR_IN clientAddr;
+			int32 addrLen = sizeof(clientAddr);
+			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+			if (clientSocket != INVALID_SOCKET)
+			{
+				cout << "Client Connected" << endl;
+
+				WSAEVENT clientEvent = ::WSACreateEvent();
+				wsaEvents.push_back(clientEvent);
+				sessions.push_back(Session{ clientSocket });
+				if (::WSAEventSelect(clientSocket, clientEvent, FD_READ | FD_WRITE | FD_CLOSE) == SOCKET_ERROR)
+					return 0;
+			}
 		}
 
-		cout << "Client Connected" << endl;
-
-		// Recv
-		while (true)
+		// Client Session Socket Check
+		if (networkEvents.lNetworkEvents & FD_READ || networkEvents.lNetworkEvents & FD_WRITE)
 		{
-			char recvBuffer[1000];
-			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-			if (recvLen == SOCKET_ERROR)
-			{
-				// 원래 블록했어야 하는 상황인데, 논블로킹으로 설정을 했다면
-				if (::WSAGetLastError() == WSAEWOULDBLOCK)
-					continue;
+			// Error-Check
+			if ((networkEvents.lNetworkEvents & FD_READ) && (networkEvents.iErrorCode[FD_READ_BIT] != 0))
+				continue;
 
-				// Error
-				break;
-			}
-			else if (recvLen == 0)
-			{
-				// 연결 끊김
-				break;
-			}
+			// Error-Check
+			if ((networkEvents.lNetworkEvents & FD_WRITE) && (networkEvents.iErrorCode[FD_WRITE_BIT] != 0))
+				continue;
 
-			cout << "Recv Data Len = " << recvLen << endl;
+			Session& s = sessions[Idx];
 
-			// Send
-			while (true)
+			// Read
+			if (s.recvBytes == 0)
 			{
-				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR)
+				int32 recvLen = ::recv(s.socket, s.recvBuffer, BUFSIZE, 0);
+				if (recvLen == SOCKET_ERROR && ::WSAGetLastError() != WSAEWOULDBLOCK)
 				{
-					// 원래 블록했어야 하는 상황인데, 논블로킹으로 설정을 했다면
-					if (::WSAGetLastError() == WSAEWOULDBLOCK)
-						continue;
-
-					// Error
-					break;
+					// TODO : Remove Session
+					continue;
 				}
 
-				cout << "Send Data Len = " << recvLen << endl;
-				break;
+				s.recvBytes = recvLen;
+				cout << "Recv Data = " << recvLen << endl;
 			}
+
+			// Write
+			if (s.recvBytes > s.sendBytes)
+			{
+				int32 sendLen = ::send(s.socket, &s.recvBuffer[s.sendBytes], s.recvBytes - s.sendBytes, 0);
+				if (sendLen == SOCKET_ERROR && ::WSAGetLastError() != WSAEWOULDBLOCK)
+				{
+					// TODO : Remove Session
+					continue;
+				}
+
+				s.sendBytes += sendLen;
+				if (s.recvBytes == s.sendBytes)
+				{
+					s.recvBytes = 0;
+					s.sendBytes = 0;
+				}
+
+				cout << "Send Data = " << sendLen << endl;
+			}
+		}
+
+		// FD_CLOSE
+		if (networkEvents.lNetworkEvents & FD_CLOSE)
+		{
+
 		}
 	}
 
